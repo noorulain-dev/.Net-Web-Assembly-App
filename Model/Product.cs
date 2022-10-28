890a2e2a1a53402e2f76fc3fd5ff3ab712424ec9
Noor ul Ain Khurshid <80438950+noorulain-dev@users.noreply.github.com>
2022-10-28 04:03:31 +0300

    Add files via upload

---
+++ Model/Product.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace P2Webasm.Model
+{
+    public class Product
+    {
+
+        public Guid Id { get; set; }
+        public String Name { get; set; }
+        public String Desc { get; set; }
+        public String Colour { get; set; }
+        public double Price { get; set; }
+        public String Image { get; set; }
+    }
+
+
+}
